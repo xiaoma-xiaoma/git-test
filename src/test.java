74bf970a297a4f2b8194f8eac0c822dfe2f370ec
xiaoma-xiaoma <65391289+xiaoma-xiaoma@users.noreply.github.com>
2022-03-22 13:57:34 +0800

    pull

--- src/test.java
+++ src/test.java
@@ -2,6 +2,7 @@ public class test {
     public static void mian(String[] args){
         System.out.println("123");
         System.out.println("push");
+        System.out.println("pull");
     }
 
 }
